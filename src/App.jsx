39e59efe6a0fbfd72dc18b56614cc397ba45dbf3
forhadhossen1 <mr.forhad1030@gamil.com>
2024-01-27 22:40:17 +0600

    mode change code

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
 
+import { useState } from 'react'
 import './App.css'
+import { useEffect } from 'react';
 
 function App() {
+  const [darkMoode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    if(darkMoode){
+      document.documentElement.classList.add('dark')
+    }else{
+      document.documentElement.classList.remove('dark')
+    }
+  }, [darkMoode])
 
   return (
     <>
-      <h1 className='text-center font-bold text-4xl '>Black & Whit</h1>
+      <div className='dark:bg-purple-400'>
+        <h1 className='text-center font-bold text-4xl '>Black & Whit</h1>
 
-      <div className='flex gap-2  min-h-[50vh] items-center align-middle'>
+        <div className='flex my-8 gap-2 items-center align-middle'>
 
-        <p className='border rounded-md p-6 bg-gray-200'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta aut, dolores dolorem rem, facilis ducimus reiciendis delectus suscipit fugiat obcaecati necessitatibus sit vitae perferendis odio magni saepe consectetur nulla illo culpa illum neque quibusdam! Voluptatibus iure laudantium aliquam, veritatis nemo accusamus quidem, sint quas, magnam dolorum dignissimos neque illo voluptatem.</p>
+          <p className='border rounded-md p-6 bg-gray-200'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta aut, dolores dolorem rem, facilis ducimus reiciendis delectus suscipit fugiat obcaecati necessitatibus sit vitae perferendis odio magni saepe consectetur nulla illo culpa illum neque quibusdam! Voluptatibus iure laudantium aliquam, veritatis nemo accusamus quidem, sint quas, magnam dolorum dignissimos neque illo voluptatem.</p>
 
-        <p className='border rounded-md p-6 bg-gray-200'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta aut, dolores dolorem rem, facilis ducimus reiciendis delectus suscipit fugiat obcaecati necessitatibus sit vitae perferendis odio magni saepe consectetur nulla illo culpa illum neque quibusdam! Voluptatibus iure laudantium aliquam, veritatis nemo accusamus quidem, sint quas, magnam dolorum dignissimos neque illo voluptatem.</p>
+          <p className='border rounded-md p-6 bg-gray-200'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta aut, dolores dolorem rem, facilis ducimus reiciendis delectus suscipit fugiat obcaecati necessitatibus sit vitae perferendis odio magni saepe consectetur nulla illo culpa illum neque quibusdam! Voluptatibus iure laudantium aliquam, veritatis nemo accusamus quidem, sint quas, magnam dolorum dignissimos neque illo voluptatem.</p>
+        </div>
+
+        <button 
+        onClick={() => {
+          setDarkMode(!darkMoode)
+        }}
+        className='p-5 bg-gray-500 rounded-lg dark:bg-gray-300'>
+          { !darkMoode ? 'Light' : 'Dark'} Mood </button>
       </div>
     </>
   )
